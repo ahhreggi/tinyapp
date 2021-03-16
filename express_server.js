9ee8bf5911363928929047103fac70f57acb3cea
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-16 14:08:17 -0400

    Add login endpoint

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -14,6 +15,9 @@ app.set("view engine", "ejs");
 
 // Convert the request body from a Buffer into a readable string (req.body)
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Parse cookies
+app.use(cookieParser());
 
 // UTILITY FUNCTIONS ///////////////////////////////
 
@@ -52,6 +56,13 @@ const generateRandomString = (length = 6) => {
 
 // ROUTES //////////////////////////////////////////
 
+// Log the user in
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
 // Redirect valid /u/shortURL requests to its longURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
@@ -60,7 +71,9 @@ app.get("/u/:shortURL", (req, res) => {
 
 // Form to create a new URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const cookieUsername = req.cookies["username"];
+  const templateVars = { username: cookieUsername };
+  res.render("urls_new", templateVars);
 });
 
 // Create a new URL
@@ -97,14 +110,18 @@ app.post("/urls/:shortURL", (req, res) => {
 
 // Display a URL from the database
 app.get("/urls/:shortURL", (req, res) => {
+  const cookieUsername = req.cookies["username"];
   const shortURL = req.params.shortURL;
-  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};
+  const templateVars = { username: cookieUsername, shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
 
 // Display all URLs in the database
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const cookieUsername = req.cookies["username"];
+  console.log("hey look!");
+  console.log(cookieUsername);
+  const templateVars = { username: cookieUsername, urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
