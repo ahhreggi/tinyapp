411241c47760a0df2b3ae18a7ad72902fc82501d
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-17 13:09:21 -0400

    Update memory db to contain hashed passwords

--- express_server.js
+++ express_server.js
@@ -22,15 +22,15 @@ const urlDatabase = {
   }
 };
 const users = {
-  "user01": {
-    id: "user01",
-    email: "user1@example.com",
-    password: "password1"
+  'aUA4CE': {
+    id: 'aUA4CE',
+    email: 'user1@example.com',
+    password: '$2b$10$Ohnf9u6HTv13.FnN5DPDs.xetN927Id./C90YXXOgREKq/hIQesiq'
   },
-  "user02": {
-    id: "user02",
-    email: "user2@example.com",
-    password: "password2"
+  ccLPCa: {
+    id: 'ccLPCa',
+    email: 'user2@example.com',
+    password: '$2b$10$yI6TVKpNSgqGK3eaAPZDu.2YIewoUuFs82bYLiYwfCUuam6cLZIHy'
   }
 };
 
