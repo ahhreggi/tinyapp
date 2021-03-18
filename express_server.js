e75d8a5df1c83ca483ef245c52dfc41bb346c43d
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-18 03:27:04 -0400

    Update user database to contain username attribute

--- express_server.js
+++ express_server.js
@@ -40,11 +40,13 @@ const urlDatabase = {
 const users = {
   "aUA4CE": {
     id: "aUA4CE",
+    username: "user1",
     email: "user1@example.com",
     password: "$2b$10$Ohnf9u6HTv13.FnN5DPDs.xetN927Id./C90YXXOgREKq/hIQesiq"
   },
   ccLPCa: {
     id: "ccLPCa",
+    username: "user2",
     email: "user2@example.com",
     password: "$2b$10$yI6TVKpNSgqGK3eaAPZDu.2YIewoUuFs82bYLiYwfCUuam6cLZIHy"
   }
